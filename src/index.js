76d03c5aa0fd6d5a339a07a6564176ed1a257df2
Jeffrey Mendez <jeffreymendez@jeffreys-mbp.home>
2017-12-10 08:53:03 -0500

    adjusting position

--- src/index.js
+++ src/index.js
@@ -234,7 +234,7 @@ class PopoverTooltip extends React.PureComponent<Props, State> {
       ],
     };
 
-    const posYDistance = Platform.OS === 'ios' ? this.state.y : this.state.y - 25;
+    const posYDistance = Platform.OS === 'ios' ? this.state.y : this.state.y - 26;
 
     const items = this.props.items.map((item, index) => {
       const classes = [ this.props.labelContainerStyle ];
